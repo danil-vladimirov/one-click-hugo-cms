67a101cdca450dd9c78afcd844c32369d87739ab
Danil Vladimirov <danilvladimirov@gmail.com>
2021-03-26 15:47:19 +0300

    added old html code

--- src/index.js
+++ src/index.js
@@ -2,5 +2,36 @@
 
 import "./css/style.scss";
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+let headline = document.querySelector('.top');
+
+headline.addEventListener('mouseover', function() {
+    headline.innerHTML = 'hovered';
+})
+
+let body = document.body;
+
+let btn = document.querySelector('.check-this');
+
+btn.addEventListener('mouseover', function(){
+    let r = Math.floor(Math.random() * 255); 
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    body.style.setProperty('background-color', 'rgb(' +r+ ',' +g+ ',' +b+ ')');
+    
+})
+
+function shuffle() {
+    let x;
+    let y;
+    let all = document.body.querySelectorAll('*');
+    
+    for (each of all) {
+        x = Math.floor(Math.random() * 15);
+        y = Math.floor(Math.random() * 15);
+    
+        // each.style.setProperty('position', 'absolute' );
+        each.style.setProperty('transform', 'rotateZ(' +x+ 'deg');
+    }
+}
+
+setTimeout(function() {shuffle()}, 3000);
